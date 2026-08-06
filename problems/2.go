468283eb7c3cfb2e562576e30aa6162610359921
Tyler Malone <19tylermalone94@gmail.com>
2026-08-06 02:17:49 -0500

    clean up nesting in prob 2

--- problems/2.go
+++ problems/2.go
@@ -8,12 +8,11 @@ func problem2() {
 	n1 := 1
 	for {
 		n := n2 + n1
-		if n <= 4000000 {
-			if n%2 == 0 {
-				sum += n
-			}
-		} else {
+		if n > 4000000 {
 			break
+		}
+		if n%2 == 0 {
+			sum += n
 		}
 		n2 = n1
 		n1 = n
