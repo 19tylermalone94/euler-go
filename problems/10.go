a0c4e73845a9fdaa42d6423f7fce620907b1d761
Tyler Malone <19tylermalone94@gmail.com>
2026-08-06 12:37:42 -0500

    add problem 10

---
+++ problems/10.go
@@ -0,0 +1,25 @@
+package problems
+
+import (
+	"euler-go/utility"
+	"fmt"
+)
+
+func init() {
+	Register(10, problem10)
+}
+
+func problem10() {
+	sum := 0
+	i := 2
+	for {
+		if i >= 2000000 {
+			break
+		}
+		if utility.IsPrime(i) {
+			sum += i
+		}
+		i++
+	}
+	fmt.Println(sum)
+}
