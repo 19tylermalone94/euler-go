8f84214d9316626a985855fb50964e5d75741495
Tyler Malone <19tylermalone94@gmail.com>
2026-08-07 01:49:16 -0500

    add problem 15

--- problems/15.go
+++ problems/15.go
@@ -1,11 +1,34 @@
 package problems
 
+import "fmt"
+
 func init() {
 	Register(15, problem15)
 }
 
 func problem15() {
-	// n := 2
-	// i, j := 0, 0
+	n := 20
+	var dp = make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	fmt.Println(dfs(0, 0, n, dp))
+}
 
+func dfs(i int, j int, n int, dp [][]int) int {
+	if i == n && j == n {
+		return 1
+	}
+	if dp[i][j] > 0 {
+		return dp[i][j]
+	}
+	total := 0
+	if i < n {
+		total += dfs(i+1, j, n, dp)
+	}
+	if j < n {
+		total += dfs(i, j+1, n, dp)
+	}
+	dp[i][j] = total
+	return total
 }
