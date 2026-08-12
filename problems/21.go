f230b2d1915e6a2f7add478bcc5e8a88b72bc8bc
Tyler Malone <19tylermalone94@gmail.com>
2026-08-11 21:07:10 -0500

    add problem 21

---
+++ problems/21.go
@@ -0,0 +1,33 @@
+package problems
+
+import "fmt"
+
+func init() {
+	Register(21, problem21)
+}
+
+func properDivisorSum(x int) int {
+	sum := 0
+	for i := 1; i <= x/2; i++ {
+		if x%i == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func isAmicable(x int) bool {
+	y := properDivisorSum(x)
+	return x != y && properDivisorSum(y) == x
+}
+
+func problem21() {
+	n := 10000
+	sum := 0
+	for i := range n {
+		if isAmicable(i) {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
+}
