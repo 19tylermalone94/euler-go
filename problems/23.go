94abf899ae310338dcddd4caeb631ae542a20e1d
Tyler Malone <19tylermalone94@gmail.com>
2026-08-12 16:17:41 -0500

    add problem 23

--- problems/23.go
+++ problems/23.go
@@ -1,19 +1,49 @@
 package problems
 
+import (
+	"euler-go/utility"
+	"fmt"
+)
+
 func init() {
 	Register(23, problem23)
 }
 
-func problem23() {
-	// sum := 0
-	// abundantNums := []int{}
-	// for i := range 28123 {
+var abundantNums []int = []int{}
 
-	// }
-	// for i := range 28123 {
-	// 	if !isSumOfTwoAbundant(i) {
-	// 		sum += i
-	// 	}
-	// }
-	// fmt.Println(sum)
+func isSumOfTwoAbundant(n int) bool {
+	for _, val1 := range abundantNums {
+		if val1 >= n {
+			break
+		}
+		for _, val2 := range abundantNums {
+			if val1+val2 > n {
+				break
+			}
+			if val1+val2 == n {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func problem23() {
+	for i := range 28124 {
+		properDivisors := utility.ProperDivisors(i)
+		sum := 0
+		for _, val := range properDivisors {
+			sum += val
+		}
+		if sum > i {
+			abundantNums = append(abundantNums, i)
+		}
+	}
+	sum := 0
+	for i := range 28124 {
+		if !isSumOfTwoAbundant(i) {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
 }
